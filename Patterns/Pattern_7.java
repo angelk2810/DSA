088e9ba1fdb2224a6dbfab0dc1f1fa066a82094b
Angellll <angelkp1102@gmail.com>
2026-07-16 00:58:14 +0530

    Deleted .vscode

--- Patterns/Pattern_7.java
+++ Patterns/Pattern_7.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class Pattern_7 {
     public static void main(String args[]){
-         Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
          int n = sc.nextInt();
          for(int i=0; i<n; i++){
             for(int j=0; j<n-i-1; j++){
