ae6c01cfee9fecbd98f3d8174b414a8a7c41907f
Angellll <angelkp1102@gmail.com>
2026-08-20 22:05:09 +0530

    Add Prime or Composite

--- Mathlogics/SmallestPrimeNumbers.java
+++ Mathlogics/SmallestPrimeNumbers.java
@@ -3,9 +3,7 @@ import java.util.*;
 public class SmallestPrimeNumbers {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         int n = sc.nextInt();
-
         int count = 0;
         int num = n + 1;
 
@@ -22,7 +20,6 @@ public class SmallestPrimeNumbers {
                     }
                 }
             }
-
             if (isPrime) {
                 System.out.println(num);
                 count++;
